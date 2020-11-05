76dde7bf830dbe1cc769b1f416cacb81686f156c
RafaelFelicioni <rafaelalmeida1995@outlook.com>
2020-11-05 02:16:40 -0300

    primeiro commit

---
+++ CursoCSharp/OO/Abstract.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.OO
+{
+    public abstract class Ifood
+    {
+        public abstract string Assistente2();
+
+        public string Tocar2() {
+            return "Trim trim trim...";
+        }
+    }
+
+    public abstract class Celular {
+        public abstract string Assistente();
+
+        public string Tocar() {
+            return "Trim trim trim...";
+        }
+    }
+
+    public class Samsung : Celular {
+        public override string Assistente() {
+            return "Olá! Meu nome é Bixby!";
+            
+        
+        }
+    }
+
+    public class IPhone : Celular {
+        public override string Assistente() {
+            return "Olá! Meu nome é Siri!";
+        }
+    }
+
+    class Abstract
+    {
+        public static void Executar() {
+            var celulares = new List<Celular> {
+                new IPhone(),
+                new Samsung()
+            };
+
+            foreach (var celular in celulares) {
+                Console.WriteLine(celular.Tocar());
+                Console.WriteLine(celular.Assistente()) ;
+            }
+        }
+    }
+}
